6870d9d4d5f6c0aae5469418b371dfe459792b06
Minio931 <d.chmielowski01@gmail.com>
2022-08-04 00:46:07 +0200

    smooth scrolling, workspace page, finishing up

--- scripts/script.js
+++ scripts/script.js
@@ -113,4 +113,11 @@ function disableOthers(activeItem){
     }
 }
 
-
+document.querySelectorAll('a[href^="#"').forEach(anchor =>{
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
